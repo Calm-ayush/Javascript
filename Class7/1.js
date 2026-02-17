3df4f5f4009708a5c1df8f1215be0f4233ea86a2
Calm-ayush <ayushrollno5@gmail.com>
2026-02-17 23:39:25 +0530

    functions

---
+++ Class7/1.js
@@ -0,0 +1,17 @@
+let a = 'JavaScript'
+let b = 10
+
+function letsLearnScope(){
+    console.log(a,b)
+    let value = false
+
+    if (true){
+        let a = 'Python'
+        let b = 20
+        let c = 30
+        let d = 40
+        value = !value
+        console.log(a,b,c,value1)}
+
+        console.log(a,b,c,value)
+}
