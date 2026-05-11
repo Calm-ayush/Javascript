a0c2834a22c9a86d5b5cb395c34d47192cf6aab8
Calm-ayush <ayushrollno5@gmail.com>
2026-05-11 23:03:21 +0530

    new

---
+++ Class14/2.js
@@ -0,0 +1,24 @@
+// program to merge and remove duplicate values from arrays
+
+function mergeUnique(arr1, arr2) {
+    let arr3 = [];
+
+    for(let i of arr1){
+        if(!arr3.includes(i)){
+            arr3.push(i);
+        }
+    }
+
+    for(let i of arr2){
+        if(!arr3.includes(i)){
+            arr3.push(i);
+        }
+    }
+
+    console.log(arr3);
+
+}
+const array1 = [1, 2, 3];
+const array2 = [2, 3, 5];
+
+mergeUnique(array1, array2);
